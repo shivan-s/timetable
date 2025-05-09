7ff41706f7d89e07f6a0053df3363fa3a417b156
Shivan Sivakumaran <me@shivan.xyz>
2025-05-09 13:03:40 +1200

    chore: fix lint

--- src/lib/dnd.ts
+++ src/lib/dnd.ts
@@ -2,17 +2,13 @@ import type { DragDropState } from '@thisux/sveltednd';
 import Item, { type ItemBase } from '$lib/items';
 import { draggable, droppable } from '@thisux/sveltednd';
 
-export const UNASSIGNED_ITEMS_CONTAINER = 'unassigned';
-
 export function handleDrop(state: DragDropState<ItemBase>) {
 	const { draggedItem, sourceContainer, targetContainer } = state;
 	const item = new Item(draggedItem);
 	if (sourceContainer === targetContainer) {
 		return;
 	}
-	if (!targetContainer || targetContainer === UNASSIGNED_ITEMS_CONTAINER) {
-		item.unassign();
-	} else {
+	if (targetContainer) {
 		item.assign(targetContainer);
 	}
 }
