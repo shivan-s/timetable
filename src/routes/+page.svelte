84a16a0ace144017ad65ee28ff4f565a2a6858ee
Shivan Sivakumaran <me@shivan.xyz>
2025-05-10 19:08:44 +1200

    chore: fix this

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,30 @@
 <script lang="ts">
 	import { crossfade, scale } from 'svelte/transition';
 	import { sineIn } from 'svelte/easing';
-	import { type Snapshot } from './$types';
-	import type Item from '$lib/items';
 	import { items } from '$lib/stores';
 	import Timetable from '$lib/components/Timetable.svelte';
+	import { page } from '$app/state';
+	import { goto } from '$app/navigation';
+	import { browser } from '$app/environment';
 
-	export const snapshot: Snapshot<Item[]> = {
-		capture: () => $items,
-		restore: (value) => items.set(value)
-	};
+	// Saves state into URL
+	if (browser) {
+		items.subscribe((value) => {
+			const data = btoa(JSON.stringify(value));
+			goto(`?data=${encodeURIComponent(data)}`);
+		});
+		const dataFromURL = page.url.searchParams.get('data');
+		if (dataFromURL) {
+			try {
+				const data = JSON.parse(atob(dataFromURL));
+				if (data) {
+					items.set(data);
+				}
+			} catch {
+				goto('/');
+			}
+		}
+	}
 
 	const [send, receive] = crossfade({ easing: sineIn, fallback: scale });
 </script>
