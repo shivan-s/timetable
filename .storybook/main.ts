6a1f97bf1ba66434b5c492d7026d425d0038ba1e
Shivan Sivakumaran <me@shivan.xyz>
2025-06-06 21:16:18 +1200

    updates storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,11 @@ import type { StorybookConfig } from '@storybook/sveltekit';
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts|svelte)'],
 	addons: [
-		'@storybook/addon-essentials',
-		'@storybook/addon-svelte-csf',
-		'@chromatic-com/storybook',
-		'@storybook/experimental-addon-test'
-	],
+        '@storybook/addon-svelte-csf',
+        '@chromatic-com/storybook',
+        '@storybook/addon-vitest',
+        '@storybook/addon-docs'
+    ],
 	framework: {
 		name: '@storybook/sveltekit',
 		options: {}
