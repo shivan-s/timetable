2290d08b49eaaac79430f992e25e1e8fcaf7c8fb
Shivan Sivakumaran <me@shivan.xyz>
2025-06-06 21:17:19 +1200

    format

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,11 @@ import type { StorybookConfig } from '@storybook/sveltekit';
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts|svelte)'],
 	addons: [
-        '@storybook/addon-svelte-csf',
-        '@chromatic-com/storybook',
-        '@storybook/addon-vitest',
-        '@storybook/addon-docs'
-    ],
+		'@storybook/addon-svelte-csf',
+		'@chromatic-com/storybook',
+		'@storybook/addon-vitest',
+		'@storybook/addon-docs'
+	],
 	framework: {
 		name: '@storybook/sveltekit',
 		options: {}
--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
 
 import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
@@ -13,19 +13,19 @@ import svelteConfig from './svelte.config.js';
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
-    includeIgnoreFile(gitignorePath),
-    js.configs.recommended,
-    ...ts.configs.recommended,
-    ...svelte.configs.recommended,
-    prettier,
-    ...svelte.configs.prettier,
-    {
+	includeIgnoreFile(gitignorePath),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs.recommended,
+	prettier,
+	...svelte.configs.prettier,
+	{
 		languageOptions: {
 			globals: { ...globals.browser, ...globals.node }
 		},
 		rules: { 'no-undef': 'off' }
 	},
-    {
+	{
 		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
 		languageOptions: {
 			parserOptions: {
@@ -36,5 +36,5 @@ export default ts.config(
 			}
 		}
 	},
-    storybook.configs["flat/recommended"]
+	storybook.configs['flat/recommended']
 );
